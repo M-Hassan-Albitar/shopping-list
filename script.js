ba19dbbb1b07529ec7136267fb0dceed2dfa1896
cell-tech <m_albitar@hotmail.com>
2023-05-11 14:43:23 +0300

    big mintaince now we can delete and search and clear all th list

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const form = document.getElementById("item-form");
 const inputItem = document.getElementById("item-input");
 const itemsList = document.querySelector("ul");
+const clearBtn = document.getElementById("clear");
+const filter = document.getElementById("filter");
 
 // Functions
+// Add new item
 function addItem(e) {
   e.preventDefault();
   const inputValue = inputItem.value;
@@ -15,6 +18,8 @@ function addItem(e) {
     itemsList.appendChild(li);
   }
   inputItem.value = "";
+  clearBtn.style.display = "block";
+  filter.style.display = "block";
 }
 
 // Create new li element
@@ -35,5 +40,45 @@ function createEle(eleName, classes) {
   return ele;
 }
 
+// Delete item
+function delItem(e) {
+  const targetElement = e.target;
+  if (targetElement.className === "fa-solid fa-xmark") {
+    targetElement.parentElement.parentElement.remove();
+  }
+  const list = document.querySelectorAll("li");
+  if (list.length === 0) {
+    clearBtn.style.display = "none";
+    filter.style.display = "none";
+  }
+}
+
+// Delete all items
+function delAllItems() {
+  const items = document.querySelectorAll("li");
+  while (itemsList.firstChild) {
+    itemsList.firstChild.remove();
+  }
+  clearBtn.style.display = "none";
+  filter.style.display = "none";
+}
+
+// Filter the items list
+function filterList() {
+  const search = filter.value.toLowerCase();
+  const items = document.querySelectorAll("li");
+  items.forEach((item) => {
+    const txt = item.textContent.toLocaleLowerCase();
+    if (txt.indexOf(search) > -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
 // Events
 form.addEventListener("submit", addItem);
+itemsList.addEventListener("click", delItem);
+clearBtn.addEventListener("click", delAllItems);
+filter.addEventListener("input", filterList);
